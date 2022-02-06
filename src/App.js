2d2ca279da363302d9fa01d534dda1543246161d
Ihor Kytsak <kitsak735@gmail.com>
2022-02-06 13:55:40 +0200

    git-pages

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div>
       <Header />
       <Routes>
-        <Route index element={<Home />} />
+        <Route path='/' element={<Home />} />
         <Route path='/catalog' element={<Catalog />} />
         <Route path='/cart' element={<Cart />} />
       </Routes>
