8de84f5a708c1e3a871f832c169ba1874bef5437
Ihor Kytsak <kitsak735@gmail.com>
2022-02-08 22:51:48 +0200

    filter item list

--- src/components/Catalog/CatalogFilter.js
+++ src/components/Catalog/CatalogFilter.js
@@ -1,36 +1,77 @@
+import { useState } from 'react';
 import Container from '../../Container/Container';
 import Select from '../UI/Select';
 import MyButton from '../UI/MyButton';
 import './CatalogFilter.css';
 
 const filterOption1 = [
-  { value: 'Filter 1' },
-  { value: '2019' },
-  { value: '2020' },
-  { value: '2021' },
-  { value: '2022' }
+  { value: '', name: 'Filter by year' },
+  { value: '', name: 'All years' },
+  { value: '2019', name: '2019' },
+  { value: '2020', name: '2020' },
+  { value: '2021', name: '2021' },
+  { value: '2022', name: '2022' }
 ];
 const filterOption2 = [
-  { value: 'Filter 2' },
-  { value: 'Xiaomi' },
-  { value: 'Apple' },
-  { value: 'Samsung' }
+  { value: '', name: 'Filter by model' },
+  { value: '', name: 'All models' },
+  { value: 'Apple', name: 'Apple' },
+  { value: 'Samsung', name: 'Samsung' },
+  { value: 'Xiaomi', name: 'Xiaomi' },
+  { value: 'Blackview', name: 'Blackview' }
 ];
 const filterOption3 = [
-  { value: 'Filter 3' },
-  { value: 'Cheap' },
-  { value: 'Expensive' }
+  { value: '', name: 'Filter by prices' },
+  { value: '', name: 'All price' },
+  { value: 'less', name: 'Up to 600' },
+  { value: 'more', name: 'More than 600' }
 ];
 
-const CatalogFilter = () => {
+const CatalogFilter = (props) => {
+  const [filteredYear, setFilteredYear] = useState('');
+  const [filteredModel, setFilteredModel] = useState('');
+  const [filteredPrice, setFilteredPrice] = useState('');
+
+  const dropdownChangeYearHandler = (event) => {
+    setFilteredYear(event.target.value);
+  };
+
+  const dropdownChangeModelHandler = (event) => {
+    setFilteredModel(event.target.value);
+  };
+
+  const dropdownChangePriceHandler = (event) => {
+    setFilteredPrice(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const itemData = {
+      year: filteredYear,
+      model: filteredModel,
+      price: filteredPrice
+    };
+    props.onSaveItemData(itemData);
+  };
+
   return (
     <div className='catalogFilter'>
       <Container>
-        <form className='catalogFilter__form'>
+        <form onSubmit={submitHandler} className='catalogFilter__form'>
           <div>
-            <Select items={filterOption1} defaultValue='Filter 1' />
-            <Select items={filterOption2} defaultValue='Filter 2' />
-            <Select items={filterOption3} defaultValue='Filter 3' />
+            <Select
+              items={filterOption1}
+              onChange={dropdownChangeYearHandler}
+            />
+            <Select
+              items={filterOption2}
+              onChange={dropdownChangeModelHandler}
+            />
+            <Select
+              items={filterOption3}
+              onChange={dropdownChangePriceHandler}
+            />
           </div>
           <MyButton type='submit' className='catalogFilter__btn'>
             Apply
--- src/components/UI/Select.js
+++ src/components/UI/Select.js
@@ -2,18 +2,18 @@ import './Select.css';
 
 const Select = (props) => {
   return (
-    <select className='select' defaultValue={props.defaultValue}>
+    <select className='select' onChange={props.onChange}>
       {props.items.map((item, index) => {
         if (index === 0) {
           return (
-            <option key={item.value} value={item.value} hidden>
-              {item.value}
+            <option key={item.name} value={item.value} hidden>
+              {item.name}
             </option>
           );
         }
         return (
           <option key={item.value} value={item.value}>
-            {item.value}
+            {item.name}
           </option>
         );
       })}
